7dbac2145b7b3de397cdd47ab061dd80dae7bd90
Chad Kunde <Kunde21@gmail.com>
2017-10-02 15:07:47 -0700

    asm/f64: Replace default gemvT kernel with col-major design.

--- internal/asm/f64/ge_amd64.go
+++ internal/asm/f64/ge_amd64.go
@@ -9,10 +9,7 @@ package f64
 // Ger performs the rank-one operation
 //  A += alpha * x * y^T
 // where A is an m×n dense matrix, x and y are vectors, and alpha is a scalar.
-func Ger(m, n uintptr, alpha float64,
-	x []float64, incX uintptr,
-	y []float64, incY uintptr,
-	a []float64, lda uintptr)
+func Ger(m, n uintptr, alpha float64, x []float64, incX uintptr, y []float64, incY uintptr, a []float64, lda uintptr)
 
 // GemvN computes
 //  y = alpha * a * x + beta * y
