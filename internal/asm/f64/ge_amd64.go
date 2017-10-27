8aed8a57d98057f031ad5cc977b72e896449d532
Chad Kunde <Kunde21@gmail.com>
2017-10-27 16:22:04 -0700

    asm/f64: Formatting fixes in ge_amd64.go

--- internal/asm/f64/ge_amd64.go
+++ internal/asm/f64/ge_amd64.go
@@ -14,19 +14,9 @@ func Ger(m, n uintptr, alpha float64, x []float64, incX uintptr, y []float64, in
 // GemvN computes
 //  y = alpha * a * x + beta * y
 // where A is an m×n dense matrix, x and y are vectors, and alpha and beta are scalars.
-func GemvN(m, n uintptr,
-	alpha float64,
-	a []float64, lda uintptr,
-	x []float64, incX uintptr,
-	beta float64,
-	y []float64, incY uintptr)
+func GemvN(m, n uintptr, alpha float64, a []float64, lda uintptr, x []float64, incX uintptr, beta float64, y []float64, incY uintptr)
 
 // GemvT computes
 //  y = alpha * A^T * x + beta * y
 // where A is an m×n dense matrix, x and y are vectors, and alpha and beta are scalars.
-func GemvT(m, n uintptr,
-	alpha float64,
-	a []float64, lda uintptr,
-	x []float64, incX uintptr,
-	beta float64,
-	y []float64, incY uintptr)
+func GemvT(m, n uintptr, alpha float64, a []float64, lda uintptr, x []float64, incX uintptr, beta float64, y []float64, incY uintptr)
